4b976cea8f70426ccbdfe83e4ee31ff6e7d5d98d
ian <iansangines@gmail.com>
2016-03-18 18:31:59 +0100

    Specify DomainEditController

--- src/main/java/domainControllers/DomainEditController.java
+++ src/main/java/domainControllers/DomainEditController.java
@@ -1,4 +1,46 @@
 package main.java.domainControllers;
 
+import main.java.domain.nodes.Author;
+import main.java.domain.nodes.Conference;
+import main.java.domain.nodes.Paper;
+import main.java.domain.nodes.Term;
+
+import java.util.HashMap;
+
 public class DomainEditController {
+    HashMap<Integer, Author> authors;
+    HashMap<Integer, Paper> papers;
+    HashMap<Integer, Conference> conferences;
+    HashMap<Integer, Term> terms;
+
+    public DomainEditController() {}
+
+    public void readAuthorsFromFile(){}
+
+    public void readPapersFromFile(){}
+
+    public void readConferencesFromFile(){}
+
+    public void readTermsFromFile(){}
+
+
+    public void writeAuthorToFile(Author author){}
+
+    public void writePaperToFile(Paper paper){}
+
+    public void writeConferenceToFile(Conference conference){}
+
+    public void writeTermToFile(Term term){}
+
+
+    public void deleteAuthorFromFile(Author author){}
+
+    public void deletePaperFromFile(Paper paper){}
+
+    public void deleteConferenceFromFile(Conference conference){}
+
+    public void deleteTermFromFile(Term term){}
+
+
+
 }
