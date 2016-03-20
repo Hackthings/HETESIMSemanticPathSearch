713203c53a8d0974fdef4d4537362427e84c1a47
Nico152 <nbafundi@yahoo.es>
2016-03-20 11:38:17 +0100

    Implement some public functions of DomainMainController

--- src/main/java/domainControllers/DomainMainController.java
+++ src/main/java/domainControllers/DomainMainController.java
@@ -45,7 +45,7 @@ public class DomainMainController {
 
             for(Author aut : pa.values()) {
 
-                paperauthor[aut.getId()][pap.getId()] = 1;
+                paperauthor[pap.getId()][aut.getId()] = 1;
             }
         }
 
@@ -53,18 +53,63 @@ public class DomainMainController {
     }
 
     public int[][] getTermPaperMatrix() {
-        return null;
+        int[][] termpaper = new int[15000][750000];
+
+        for(Term term : terms.values()){
+
+            HashMap<Integer, Paper> termpap = term.getPapersWhichTalkAboutThis();
+
+            for( Paper pap : termpap.values()){
+
+                termpaper[term.getId()][pap.getId()] = 1;
+            }
+        }
+
+        return termpaper;
     }
 
     public int[][] getPaperTermMatrix() {
-        return null;
+        int[][] paperterm = new int[750000][15000];
+
+        for (Paper pap : papers.values()) {
+
+            HashMap<Integer, Term> papterm = pap.getTerms();
+
+            for( Term term : papterm.values()){
+
+                paperterm[pap.getId()][term.getId()] = 1;
+            }
+        }
+
+        return paperterm;
     }
 
     public int[][] geConferencePaperMatrix() {
-        return null;
+        int[][] confpaper = new int[5000][750000];
+
+        for(Conference conf : conferences.values()){
+
+            HashMap<Integer, Paper> confpap = conf.getExposedPapers();
+
+            for( Paper pap : confpap.values()){
+
+                confpaper[conf.getId()][pap.getId()] = 1;
+            }
+        }
+
+        return confpaper;
     }
 
     public int[][] getPaperConferenceMatrix() {
-        return null;
+        int[][] paperconf = new int[750000][5000];
+
+        for(Paper pap : papers.values()){
+
+            Conference conf = pap.getConference();
+
+            paperconf[pap.getId()][conf.getId()] = 1;
+        }
+
+        return paperconf;
     }
 }
