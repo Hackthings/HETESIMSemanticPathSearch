d75fd723186585ee045bee97f190d75c5d2a90e0
Andreu Rodríguez i Donaire <arodriguezdonaire@gmail.com>
2016-04-05 14:30:35 +0200

    Implement domain global controller

--- src/main/java/domainControllers/DomainGlobalController.java
+++ src/main/java/domainControllers/DomainGlobalController.java
@@ -1,33 +1,23 @@
 package main.java.domainControllers;
 
-import main.java.presentation.ConsolePrinter;
-
-import java.util.Scanner;
-
 public class DomainGlobalController extends DomainMainController {
-    private ConsolePrinter consolePrinter;
-
     public DomainGlobalController() {
-        consolePrinter = new ConsolePrinter();
+
     }
 
     public void newQuery() {
-        Scanner sc = new Scanner(System.in);
-
         DomainHetesimController hetesimController = new DomainHetesimController(getAuthorPaperMatrix(),
                                                                                 getPaperAuthorMatrix(),
                                                                                 getTermPaperMatrix(),
                                                                                 getPaperTermMatrix(),
                                                                                 getConferencePaperMatrix(),
                                                                                 getPaperConferenceMatrix());
-
-        consolePrinter.printQueryInputMessage();
-        String query = sc.nextLine();
-        hetesimController.heteSim(query);
+        hetesimController.newQuery();
     }
 
     public void editGraph() {
-        // TODO: Implement when Ian finishes the DomainEditController.
+        DomainEditController domainEditController = new DomainEditController();
+        domainEditController.newEdit();
     }
 
 }
