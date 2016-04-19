88baa748aa5f684ea4e85261290b2bf45548c2cc
Nico152 <nbafundi@yahoo.es>
2016-04-19 14:24:27 +0200

    Add functions getXYmatrix at DomainMainController with the  new Object Matrix

--- src/main/java/ownClasses/domain/domainControllers/DomainMainController.java
+++ src/main/java/ownClasses/domain/domainControllers/DomainMainController.java
@@ -1,5 +1,6 @@
 package main.java.ownClasses.domain.domainControllers;
 
+import main.java.sharedClasses.utils.Matrix;
 import main.java.sharedClasses.utils.Pair;
 import main.java.sharedClasses.domain.domainControllers.DomainPersistanceController;
 import main.java.sharedClasses.domain.nodes.Author;
@@ -45,6 +46,78 @@ public class DomainMainController {
     public void editGraph() {
         DomainPersistanceController domainPersistanceController = new DomainPersistanceController();
         domainPersistanceController.newEdit(authorsById, papersById, conferencesById, termsById, authorsByName, papersByName, conferencesByName,termsByName, authorMaxId, paperMaxId, termMaxId, conferenceMaxId);
+    }
+
+    public Matrix getAuthorPaperMatrix(){
+        Matrix authorpaper = new Matrix();
+
+        for(Author author : authorsById.values()){
+            HashMap<Integer,Paper> papersOfAuthor = author.getPapersById();
+            for(Paper paper : papersOfAuthor.values()){
+                authorpaper.addValue(author.getId(),paper.getId(),1.0);
+            }
+        }
+        return authorpaper;
+    }
+
+    public Matrix getPaperAuthorMatrix(){
+        Matrix paperauthor = new Matrix();
+
+        for(Paper paper : papersById.values()){
+            HashMap<Integer,Author> authorsOfPaper = paper.getAuthorsById();
+            for(Author author : authorsOfPaper.values()){
+                paperauthor.addValue(paper.getId(),author.getId(),1.0);
+            }
+        }
+        return paperauthor;
+    }
+
+    public Matrix getTermPaperMatrix(){
+        Matrix termpaper = new Matrix();
+
+        for(Term term : termsById.values()){
+            HashMap<Integer,Paper> papersOfTerm = term.getPapersWhichTalkAboutThisById();
+            for(Paper paper : papersOfTerm.values()){
+                termpaper.addValue(term.getId(),paper.getId(),1.0);
+            }
+        }
+
+        return termpaper;
+    }
+
+    public Matrix getPaperTermMatrix() {
+        Matrix paperterm = new Matrix();
+
+        for (Paper paper : papersById.values()) {
+            HashMap<Integer, Term> termsOfPaper = paper.getTermsById();
+            for (Term term : termsOfPaper.values()) {
+                paperterm.addValue(paper.getId(), term.getId(), 1.0);
+            }
+        }
+        return paperterm;
+    }
+
+    public Matrix getConferencePaperMatrix(){
+        Matrix conferencepaper = new Matrix();
+
+        for(Conference conf : conferencesById.values()){
+            HashMap<Integer,Paper> papersOfConf = conf.getExposedPapersById();
+            for(Paper paper : papersOfConf.values()){
+                conferencepaper.addValue(conf.getId(),paper.getId(),1.0);
+            }
+        }
+
+        return conferencepaper;
+    }
+
+    public Matrix getPaperConferenceMatrix(){
+        Matrix paperconference = new Matrix();
+
+        for(Paper paper : papersById.values()){
+            paperconference.addValue(paper.getId(),paper.getConference().getId(),1.0);
+        }
+
+        return paperconference;
     }
 
 }
