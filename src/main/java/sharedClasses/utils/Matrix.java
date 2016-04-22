78a9d1c22e6a41c680c8a02243ddfe8fa45df6ab
Andreu Rodríguez i Donaire <arodriguezdonaire@gmail.com>
2016-04-22 11:26:42 +0200

    Update matrix

--- src/main/java/sharedClasses/utils/Matrix.java
+++ src/main/java/sharedClasses/utils/Matrix.java
@@ -1,70 +1,87 @@
 package main.java.sharedClasses.utils;
 
 
-import java.util.ArrayList;
+import java.util.Set;
+import java.util.LinkedList;
 import java.util.HashMap;
-import java.util.Iterator;
-
 
 public class Matrix {
-    private HashMap<Integer, ArrayList<Pair<Integer, Double>>> matrix; //aquest es el format de la matriu que va dir el borja, podem discutir altres si us es mes facil o ho creieu millor. En la nostra practica Double sera segurament Double
+    private HashMap<Integer, LinkedList<Pair<Integer, Double>>> matrix; //aquest es el format de la matriu que va dir el borja, podem discutir altres si us es mes facil o ho creieu millor. En la nostra practica Double sera segurament Double
+    private HashMap<Integer, LinkedList<Pair<Integer, Double>>> mTrans;
 
     //Constructora de la classe
     //Pre: Cert
     //Post: Crea una matriu buida i inicialitza el pair
     public Matrix() {
-        this.matrix = new HashMap<Integer, ArrayList<Pair<Integer, Double>>>();
+        this.matrix = new HashMap<Integer, LinkedList<Pair<Integer, Double>>>();
+        this.mTrans = new HashMap<Integer, LinkedList<Pair<Integer, Double>>>();
+    }
+
+    public Matrix(HashMap<Integer, LinkedList<Pair<Integer, Double>>> m, HashMap<Integer, LinkedList<Pair<Integer, Double>>> mTrans) {
+        this.matrix = m;
+        this.mTrans = mTrans;
     }
 
     //Pre: Cert
-    //TODO Post: Retorna un vector amb les files valides
-    public ArrayList<Integer> rows() {
-        //return this.matrix.size();
-        return null;
+    //Post: Retorna un vector amb les files valides
+    public Set<Integer> rows() {
+        return this.matrix.keySet();
     }
 
     //Pre: Cert
-    //TODO Post: Retorna un vector amb les columnes valides de la fila key
-    public ArrayList<Integer> columns(int key) {
-        /*if (this.matrix.isEmpty()) return 0;
-        else {
-            ArrayList<Pair<Integer,Double>> tmp = this.matrix.get(key);
-            return tmp.size();
-        }*/
-        return null;
+    //Post: Retorna un vector amb les columnes valides
+    public Set<Integer> cols() {
+        return this.mTrans.keySet();
     }
 
+
+    //Pre: Existeix la fila key
+    //Post: Retorna un vector amb les columnes valides de la fila key
+    public LinkedList<Pair<Integer, Double>> columns(int key) {
+        return this.matrix.get(key);
+    }
+
+
     //Pre: Cert
-    //Post: Afegeix una fila buida (un ArrayList<Pair<Integer, Double>> buit)
+    //Post: Afegeix una fila buida (un LinkedList<Pair<Integer, Double>> buit)
     public void addRow(int i){
-        this.matrix.put(i, new ArrayList<Pair<Integer, Double>>());
+        this.matrix.put(i, new LinkedList<Pair<Integer, Double>>());
     }
+
 
     //Pre: Cert
     //Post: Elimina la fila i de la matriu (elimina l'ArrayList amb id i)
     public void deleteRow(int i){
-        ArrayList<Pair<Integer,Double>> tmp = this.matrix.remove(i);
+        LinkedList<Pair<Integer, Double>> tmp = this.matrix.remove(i);
     }
+
 
     //Pre: Cert
     //Post: Afegeix el valor value a la posició i,j. Afegeix una fila si es necessari.
+    //      Si el valor es 0, comprova si cal eliminar la fila.
     public void addValue(int i, int j, double value){
-        //TODO si value es 0 comprovar si cal eliminar la fila
         if (this.matrix.containsKey(i)) {
-            Pair<Integer, Double> p = new Pair<Integer,Double>(j, value);
-            this.matrix.get(i).add(j, p);
-        } else {
+            if (value == 0) {
+                this.matrix.get(i).remove(j);
+                this.mTrans.get(j).remove(i);
+            }
+            else {
+                this.matrix.get(i).add(j, new Pair<Integer, Double>(j, value));
+                this.mTrans.get(j).add(i, new Pair<Integer, Double>(i, value));
+            }
+        } else if (value != 0) {
             addRow(i);
-            this.matrix.get(i).add(j, new Pair<Integer,Double>(j, value));
+            this.matrix.get(i).add(j, new Pair<Integer, Double>(j, value));
+            this.mTrans.get(j).add(i, new Pair<Integer, Double>(i, value));
         }
-
     }
+
 
     //Pre: Cert.
     //Post: Retorna el valor de i, j, si existeix, sino retorna -1.
     public double getValue(int i, int j){
         if (this.matrix.containsKey(i)) {
-            ArrayList<Pair<Integer, Double>> list = this.matrix.get(i);
+            LinkedList<Pair<Integer, Double>> list = this.matrix.get(i);
             if (list.contains(j)) {
                 Pair<Integer, Double> p = list.get(i);
                 return p.getSecond();
@@ -73,25 +90,11 @@ public class Matrix {
         } else return -1;
     }
 
-    //TODO divideix totes les files per el nombre de valors
-    //Pre: la matriu conte nomes 1s i 0s (no cal tirar excepcio)
-    public Matrix normalize(){
-
-    }
 
     //Pre: Cert
     //Post: Retorna la matriu transposada.
     public Matrix transpose(){
-        Matrix m = new Matrix();
-        Iterator<Integer> it = this.matrix.keySet().iterator();
-        int fila;
-        while (it.hasNext()) {
-            fila = it.next();
-            ArrayList<Pair<Integer, Double>> list = this.matrix.get(fila);
-            for (Pair col : list) {
-                m.addValue(col.getFirst(), fila, col.getSecond());
-            }
-        }
+        Matrix m = new Matrix(this.mTrans, this.matrix);
         return m;
     }
 
@@ -99,9 +102,34 @@ public class Matrix {
     //Pre: el paràmetre implícit té el mateix nombre de columnes que files té m
     //Post: retorna la matriu producte del paràmetre implícit amb m
     public Matrix multiply(Matrix m){
+
+        try {
+            if (rows().size() == m.rows().size()) {
+                throw new Exception("No es poden multiplicar les matrius!");
+            }
+        } catch (Exception error) {
+
+        }
         //TODO comprovar el Pre i llançar excepcio
         Matrix mult = new Matrix();
-        for (int x = 0; x < rows().size(); ++x) {
+        Set<Integer> files = rows();
+        Set<Integer> columnes = m.cols();
+
+        for (int x : files) {
+            for (int y : columnes) {
+                double valor = 0;
+                for (int z = 0; z < columns(x).size(); z++) {
+                    double val1 = getValue(x, z);
+                    double val2 = m.getValue(z, y);
+                    if (val1 != -1 && val2 != -1) {
+                        valor += val1 * val2;
+                    }
+                }
+                mult.addValue(x, y, valor);
+            }
+        }
+
+        /*for (int x = 0; x < rows().size(); ++x) {
             for (int y = 0; y < m.colums(x).size(); y++) {
                 double valor = 0;
                 for (int z = 0; z < colums(x).size(); z++) {
@@ -109,23 +137,54 @@ public class Matrix {
                     double val2 = m.getValue(z, y);
                     if (val1 == -1) val1 = 0;
                     if (val2 == -1) val2 = 0;
-                    valor += val1*val2;
+                    valor += val1 * val2;
                 }
                 mult.addValue(x, y, valor);
             }
-        }
+        }*/
         return mult;
     }
 
     //Pre: Cert
-    //Post: retorna el modul de la fila i.
-    public double modulus(int i) {
-        //si la fila no existeix retorna 0
-        ArrayList<Pair<Integer, Double>> list = this.matrix.get(i);
-        double m = 0;
-        for (Pair par : list) {
-            m += par.getSecond()*par.getSecond();
-        }
-        return Math.sqrt(m);
+    //Post: retorna el modul de la fila i. Si no existeix, retorna 0.
+    public double rowModulus(int i) {
+        if (this.matrix.containsKey(i)) {
+            LinkedList<Pair<Integer, Double>> list = this.matrix.get(i);
+            double m = 0;
+            for (Pair par : list) {
+                m += par.getSecond() * par.getSecond();
+            }
+            return Math.sqrt(m);
+        } else return 0;
     }
+
+
+    //Pre: Cert
+    //Post: retorna el modul de la columna j. Si no existeix, retorna 0.
+    public double columModulus(int j) {
+        if (this.mTrans.containsKey(j)) {
+            LinkedList<Pair<Integer, Double>> list = this.mTrans.get(j);
+            double m = 0;
+            for (Pair par : list) {
+                m += par.getSecond() * par.getSecond();
+            }
+            return Math.sqrt(m);
+        } else return 0;
+    }
+
+
+    //Pre: La matriu conte nomes 1s i 0s (no cal tirar excepcio)
+    //Post: Divideix totes les files per el nombre de valors
+    public Matrix normalize(){
+        Matrix n = new Matrix();
+        for (int i : this.matrix.keySet()) {
+            double valor = rows().size();
+            for (Pair<Integer, Double> p : this.matrix.get(i)) {
+                n.addValue(i, p.getFirst(), p.getSecond()/valor);
+            }
+        }
+        return n;
+    }
+
 }
+
