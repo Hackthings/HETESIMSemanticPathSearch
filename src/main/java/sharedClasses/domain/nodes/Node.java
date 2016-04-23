645df12de71100ab26de50f5d5dca960430a27e0
Nico152 <nbafundi@yahoo.es>
2016-04-23 10:59:13 +0200

    Add SetId at Node from Oriol

--- src/main/java/sharedClasses/domain/nodes/Node.java
+++ src/main/java/sharedClasses/domain/nodes/Node.java
@@ -11,19 +11,35 @@ public class Node {
         if (maxId < id) maxId = id;
     }
 
+    //Pre: Cert.
+    //Post: Retorna el valor de maxId del parametre implicit.
     public static int getMaxId() {
         return maxId;
     }
 
+    //Pre: Cert.
+    //Post: Retorna el nom de l'objecte.
     public String getName() {
         return name;
     }
 
+
+    //Pre: name es un string valid.
+    //Post: Modifica el nom del parametre implicit per el valor name.
     public void setName(String name) {
         this.name = name;
     }
 
+
+    //Pre: Cert.
+    //Post: Retorna l'id de l'objecte.
     public int getId() {
         return id;
+    }
+
+    //Pre: Cert.
+    //Post: Modifica l'indentificador del parametre implicit per el valor name.
+    public void setId(int id) {
+        this.id = id;
     }
 }
