529395c02dfc428e7712460b9298ff137f8c917d
iansangines <iansangines@gmail.com>
2016-04-25 18:52:26 +0200

    Insert Persistannce Driver

---
+++ src/main/java/ownClasses/domain/domainControllers/Drivers/DomainPersistanceController_Driver.java
@@ -0,0 +1,47 @@
+package main.java.ownClasses.domain.domainControllers.Drivers;
+
+
+import main.java.sharedClasses.domain.domainControllers.DomainPersistanceController;
+import main.java.sharedClasses.domain.nodes.Author;
+import main.java.sharedClasses.domain.nodes.Conference;
+import main.java.sharedClasses.domain.nodes.Paper;
+import main.java.sharedClasses.domain.nodes.Term;
+
+import java.util.*;
+
+/**
+ * Created by MarcBotet on 21/04/2016.
+ */
+public class DomainPersistanceController_Driver {
+
+    public static void main(String[] args) {
+        HashMap<Integer, Author> authorsById = new HashMap<Integer, Author>();
+        HashMap<Integer, Paper> papersById =  new HashMap<Integer, Paper>();
+        HashMap<Integer, Conference> conferencesById = new HashMap<Integer, Conference>();
+        HashMap<Integer, Term> termsById = new HashMap<Integer, Term>();
+        HashMap<String, Author> authorsByName = new HashMap<String, Author>();
+        HashMap<String, Paper> papersByName = new HashMap<String, Paper>();;
+        HashMap<String, Conference> conferencesByName = new HashMap<String, Conference>();
+        HashMap<String, Term> termsByName = new HashMap<String, Term>();
+        Author a = new Author("",-1);
+        Paper p = new Paper("",-1);
+        Term t = new Term("",-1);
+        Conference c = new Conference("",-1);
+
+
+        DomainPersistanceController dpc = new DomainPersistanceController();
+        dpc.readAll(authorsById,papersById,
+                conferencesById,termsById,
+                authorsByName,papersByName,
+                conferencesByName,
+                termsByName);
+
+
+
+        dpc.newEdit(authorsById,papersById,
+                conferencesById,termsById,
+                authorsByName,papersByName,
+                conferencesByName,
+                termsByName);
+    }
+}
