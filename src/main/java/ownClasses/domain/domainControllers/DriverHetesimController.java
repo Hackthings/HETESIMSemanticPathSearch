a4ade54e6860a8645d5a74da7cf68c45c4f0ab31
Nico152 <nbafundi@yahoo.es>
2016-04-25 19:52:07 +0200

    Add DriverHetesim (not sure if correct)

---
+++ src/main/java/ownClasses/domain/domainControllers/DriverHetesimController.java
@@ -0,0 +1,51 @@
+package main.java.ownClasses.domain.domainControllers;
+
+
+import main.java.sharedClasses.utils.Matrix;
+
+
+public class DriverHetesimController {
+
+    private static void escriu_matriu(Matrix m) {
+        for (int i : m.rows()) {
+            for (int j : m.cols()) {
+                System.out.println(" Fila " + i + ", columna " + j + ", valor: " + m.getValue(i, j));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Matrix ap = new Matrix();
+
+        Matrix tp = new Matrix();
+
+        Matrix cp = new Matrix();
+
+        for (int x = 0; x < 3; ++x) {
+            for (int y = 0; y < 2; ++y) {
+                double valor = y % 2;
+                ap.addValue(x, y, valor);
+            }
+        }
+        for (int x = 3; x < 6; ++x) {
+            for (int y = 0; y < 2; ++y) {
+                double valor = y % 2;
+                tp.addValue(x, y, valor);
+            }
+        }
+        for (int x = 6; x < 7; ++x) {
+            for (int y = 0; y < 2; ++y) {
+                double valor = y % 2;
+                cp.addValue(x, y, valor);
+            }
+        }
+
+        DomainHetesimController h = new DomainHetesimController(ap,ap.transpose(),tp,tp.transpose(),cp,cp.transpose());
+
+        Matrix resultat = h.heteSim("APAPAPCPA");
+
+        escriu_matriu(resultat);
+
+    }
+
+}
