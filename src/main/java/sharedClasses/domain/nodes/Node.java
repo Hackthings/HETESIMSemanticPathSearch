f4143bc29a9bc8023f76921481598687206b3d10
Andreu Rodríguez i Donaire <arodriguezdonaire@gmail.com>
2016-05-31 14:54:04 +0200

    Comment Node class for JavaDoc

--- src/main/java/sharedClasses/domain/nodes/Node.java
+++ src/main/java/sharedClasses/domain/nodes/Node.java
@@ -6,35 +6,62 @@ public class Node implements Serializable{
     private int id;
     private String name;
 
-
+    /**
+     * Constructor function for the Node class.
+     * <p>
+     * The Node class represents a node in our graph.
+     *
+     * @param  name the name of the conference
+     * @param  id the id of the conference
+     * @see         Node
+     */
     public Node(String name, int id) {
         this.name = name;
         this.id = id;
     }
 
 
-    //Pre: Cert.
-    //Post: Retorna el nom de l'objecte.
+    /**
+     * Returns the name of the node
+     *
+     * @return      the name of the node
+     * @see         String
+     */
     public String getName() {
         return name;
     }
 
 
-    //Pre: name es un string valid.
-    //Post: Modifica el nom del parametre implicit per el valor name.
+
+    /**
+     * Sets the name of the node
+     *
+     * @param name the name of the node to be set
+     * @see         String
+     */
     public void setName(String name) {
         this.name = name;
     }
 
 
-    //Pre: Cert.
-    //Post: Retorna l'id de l'objecte.
+
+    /**
+     * Returns the id of the node
+     *
+     * @return      the id of the node
+     * @see         String
+     */
     public int getId() {
         return id;
     }
 
-    //Pre: Cert.
-    //Post: Modifica l'indentificador del parametre implicit per el valor name.
+
+    /**
+     * Sets the id of the node
+     *
+     * @param id the id of the node to be set
+     * @see         String
+     */
     public void setId(int id) {
         this.id = id;
     }
